2d39459488ae1229e2eb819b63af1719ca4da211
Andrew McGivery <andrew@mcgivery.com>
2013-05-20 17:09:20 -0400

    Added Final Code

---
+++ requesthelper.js
@@ -0,0 +1,48 @@
+var RequestHelper = {
+	GatewayURL: "https://www.yourdomain.on.ca/secure/gateway.html",
+	Busy: false,
+	sendRequest: function(url,success,$){
+		var protocol = location.protocol;
+		if(window.XDomainRequest){
+			if(protocol == "http:"){
+				if(RequestHelper.Busy){
+					setTimeout(function(){
+						RequestHelper.sendRequest(url,success,$);
+					},50);
+				} else {
+					RequestHelper.Busy = true;
+					$("body").append("<iframe id="ajaxProxy" style="display: none;" src="&quot;+RequestHelper.GatewayURL+&quot;" height="240" width="320"></iframe>");
+					$("#ajaxProxy").load(function(){
+						ajaxProxy.postMessage(url,"*");
+						$(window).bind("message",function(e){
+							$("#ajaxProxy").remove();
+							$(window).unbind("message");
+							RequestHelper.Busy = false;
+							success(e.originalEvent.data);
+						});
+					});
+				}
+			} else {
+				var xdr = new XDomainRequest();
+				xdr.open("get", url);
+				xdr.onprogress = function () { };
+				xdr.ontimeout = function () { };
+				xdr.onerror = function () { };
+				xdr.onload = function() {
+				  success(xdr.responseText);
+				}
+				setTimeout(function () {xdr.send();}, 0);
+			}
+		} else {
+			$.ajax({
+				type: "GET",
+				url: url,
+				dataType: "html",
+				async:true,
+				success: function (response){
+					success(response);
+				}
+			});
+		}
+	}
+}
